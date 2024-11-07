e84f82ee8ea9c619d269b040112368195a2268f6
iossefy <git@youssefy.com>
2024-11-07 14:14:00 +0200

    add llm module

---
+++ scribe/llm.py
@@ -0,0 +1,35 @@
+import google.generativeai as genai
+import os
+
+
+class LLM:
+    def __init__(self, api_key: str = os.environ.get("GEMINI_API_KEY")):
+        self.api_key = api_key
+        genai.configure(api_key=self.api_key)
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+    def summarize(
+        self, input_text: str, max_tokens: int = 64, temperature: float = 0.3
+    ) -> str:
+        """
+        Generates a summary of the provided input text using the generative AI model.
+
+        Parameters:
+        ----------
+        input_text : str
+            The text that needs to be summarized.
+        max_tokens : int, optional
+            Maximum number of tokens in the output summary.
+        temperature : float, optional
+            The creativity level for the response.
+        """
+        system_instruction = "You are an AI that provides summaries of the input text."
+
+        response = self.model.generate_content(
+            [system_instruction, input_text],
+            generation_config=genai.GenerationConfig(
+                max_output_tokens=max_tokens,
+                temperature=temperature,
+            ),
+        )
+        return response.text
