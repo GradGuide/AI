ee6307b0afe764eeac6e925a1b2136af7eadd51e
iossefy <git@youssefy.com>
2024-11-08 17:35:58 +0200

    add imports

--- scribe/__init__.py
+++ scribe/__init__.py
@@ -1,1 +1,6 @@
-#!/usr/bin/env python
+from .qna import QnA
+from .llm import LLM
+from .summary import Summary
+from .similarity import Similarity
+
+__all__ = ["QnA", "LLM", "Summary", "Similarity"]
